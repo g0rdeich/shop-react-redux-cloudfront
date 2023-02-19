50c903e20583baeb84a431f2dbcaf5e6c02a0a72
Aleksandr Gordeev1 <aleksandr_gordeev1@epam.com>
2023-02-19 15:38:28 +0400

    complete task 2

--- src/components/pages/PageProducts/PageProducts.tsx
+++ src/components/pages/PageProducts/PageProducts.tsx
@@ -1,10 +1,14 @@
-import Products from "~/components/pages/PageProducts/components/Products";
+// import Products from "~/components/pages/PageProducts/components/Products";
 import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 
 export default function PageProducts() {
   return (
     <Box py={3}>
-      <Products />
+      <Typography sx={{ display: "flex", justifyContent: "center" }}>
+        Products will be here soon
+      </Typography>
+      {/* <Products /> */}
     </Box>
   );
 }
