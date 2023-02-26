3697d5b6798549b82b866b530142df4172d7b275
Aleksandr Gordeev1 <aleksandr_gordeev1@epam.com>
2023-02-26 16:11:26 +0400

    complete task 3, intagrate products-service with FE

--- src/components/pages/PageProducts/PageProducts.tsx
+++ src/components/pages/PageProducts/PageProducts.tsx
@@ -1,14 +1,10 @@
-// import Products from "~/components/pages/PageProducts/components/Products";
+import Products from "~/components/pages/PageProducts/components/Products";
 import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
 
 export default function PageProducts() {
   return (
     <Box py={3}>
-      <Typography sx={{ display: "flex", justifyContent: "center" }}>
-        Products will be here soon
-      </Typography>
-      {/* <Products /> */}
+      <Products />
     </Box>
   );
 }
